09cb796491a9c98569fdc8a09cf27ee960101423
Ibrahim Yusuf <hireyusufibrahim@gmail.com>
2025-03-26 08:14:25 +0100

    Linkedin landing page to app

---
+++ landingPage/landing.js
@@ -0,0 +1,42 @@
+// Grabbing element from Dom
+const logo = document.querySelector(".logo");
+
+const getStartedBtns = document.querySelectorAll(".call-to-actionBtn");
+
+const logoFooter = document.querySelector(".logoFooter");
+
+
+const openBannerPage = () => {
+    const link = document.createElement("a");
+    document.body.appendChild(link);
+    
+    link.href = "../banner.html";
+    link.click();
+    document.body.removeChild(link);
+}
+
+logo.addEventListener("click", () => {
+    openBannerPage();
+});
+
+for (let getStartedBtn of getStartedBtns) {
+    getStartedBtn.addEventListener("click", () => {
+        openBannerPage();
+    });
+}
+
+logoFooter.addEventListener("click", () => {
+    openBannerPage();
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    logo.classList.add("redirectElement");
+
+    for (let getStartedBtn of getStartedBtns) {
+        getStartedBtn.classList.add("redirectElement");
+    }
+
+    logoFooter.classList.add("redirectElement");
+})
